3b8b8df418ce65f2a01fc5676d348436878b187a
Rasul <sofiyevsr@gmail.com>
2021-10-04 13:57:24 +0400

    feat(mobile): better images, translations fix(website): i18n config fix

--- app-backend/scripts/zip_db.sh
+++ app-backend/scripts/zip_db.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-tar -czvf db-artifacts.tgz ../src/config/db ../src/utils/commons/sqlToKnex.ts ..src/utils/dbData.ts ../src/utils/constants.ts ../knexfile.ts ../package.json ../yarn.lock
+tar -czvf db-artifacts.tgz ../src/config/db ../src/utils/commons/sqlToKnex.ts ../src/utils/dbData.ts ../src/utils/constants.ts ../knexfile.ts ../package.json ../yarn.lock
--- website/next-i18next.config.js
+++ website/next-i18next.config.js
@@ -1,7 +1,9 @@
+const path = require("path")
 module.exports = {
   i18n: {
     defaultLocale: "az",
     locales: ["az", "en", "ru"],
     localeDetection: false,
+    localePath: path.resolve("./public/locales"),
   },
 };
