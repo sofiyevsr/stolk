2a269fd25a7ad8f2dcfa1c456fa5842048367e5d
Rasul <sofiyevsr@gmail.com>
2021-10-26 01:55:05 +0400

    fix ban controllers

--- app-backend/src/admin/controllers/user/ban.ts
+++ app-backend/src/admin/controllers/user/ban.ts
@@ -4,19 +4,11 @@ import Joi from "joi";
 
 async function ban(id: string | undefined) {
   const val = await Joi.number().validateAsync(id);
-  const [banned_at] = await db(tables.base_user)
-    .update({ banned_at: db.fn.now() }, ["banned_at"])
-    .where({ id: val });
-  return banned_at;
-}
-
-async function unban(id: string | undefined) {
-  const val = await Joi.number().validateAsync(id);
   const trx = await db.transaction();
   try {
     // Banned at is object of {banned_at: string}
     const [banned_at] = await trx(tables.base_user)
-      .update({ banned_at: null }, ["banned_at"])
+      .update({ banned_at: db.fn.now() }, ["banned_at"])
       .where({ id: val });
     await trx(tables.user_session).del().where({ user_id: val });
     await trx.commit();
@@ -25,6 +17,14 @@ async function unban(id: string | undefined) {
     await trx.rollback();
     throw error;
   }
+}
+
+async function unban(id: string | undefined) {
+  const val = await Joi.number().validateAsync(id);
+  const [banned_at] = await db(tables.base_user)
+    .update({ banned_at: null }, ["banned_at"])
+    .where({ id: val });
+  return banned_at;
 }
 
 export { ban, unban };
