524ded8703c7f1a418a4783a9ae757bbd581a9df
Rasul <sofiyevsr@gmail.com>
2021-12-10 23:51:59 +0400

    fix(mobile): upgrade analytics fix analytics 9.0.3 conflicts

--- feedapp/lib/utils/services/app/logger.dart
+++ feedapp/lib/utils/services/app/logger.dart
@@ -1,5 +1,4 @@
 import 'package:firebase_analytics/firebase_analytics.dart';
-import 'package:firebase_analytics/observer.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 
 class AppLogger {
