78b305f925249b44c5f8895875b28df44d965df0
Rasul <sofiyevsr@gmail.com>
2022-01-23 16:01:48 +0400

    api/v0.3.0

--- app-backend/src/config/db/views/analytics_by_overall.sql
+++ app-backend/src/config/db/views/analytics_by_overall.sql
@@ -1,8 +1,6 @@
 CREATE MATERIALIZED VIEW analytics_by_overall AS
-SELECT COUNT(DISTINCT(n.id))::INTEGER as news_count,
-COUNT(DISTINCT(u.id))::INTEGER as user_count,
+SELECT COUNT(n.id)::INTEGER as news_count,
+(SELECT COUNT(u.id) as user_count from base_user as u),
 CURRENT_TIMESTAMP as last_update
 FROM news_feed as n
-LEFT JOIN base_user as u on true
-WHERE n.created_at > CURRENT_DATE - INTERVAl '7' DAY
 WITH DATA
